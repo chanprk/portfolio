3568e6602dcfc44a9ca40f15aeb743cfc9148a0f
Chan Pruksapha <chan.pruksapha14@imperial.ac.uk>
2016-10-30 23:09:07 +0700

    NER project

---
+++ ner/app.js
@@ -0,0 +1,40 @@
+//contents = ['A','B'] 
+contents = [article1,article2];
+//titles = [title1,title2]
+
+var app = angular.module('NerApp',[]);
+
+app.controller('BodyCtrl', ['$scope', '$http', function($scope, $http){
+
+  $scope.contents = contents;
+  $scope.companies = []
+  //$scope.titles = titles;
+
+  $scope.edit = function(){
+  	//console.log($scope.contents[$scope.articleNum])
+  	$scope.textValue = $scope.contents[$scope.articleNum];
+  }
+
+  $scope.$watch('textValue', function(newValue, oldValue) {
+  	if(!newValue) return;
+
+  	console.log(newValue);
+
+  	var req = {
+  				method: 'POST',
+  				url: 'http://localhost:8080/ner',
+  				headers: {
+  					'Content-Type': undefined
+  				},
+  				data: { content: newValue }
+  	};
+
+    /* Sending request for headlines */
+  	$http(req).then(function(response){
+  		$scope.companies = response.data;
+  	})
+
+  });
+
+
+}]);
